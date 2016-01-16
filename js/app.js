ea0c99bc5b2bf19124056aca1a8b286a078e1dfc
designviacode <designviacode@gmail.com>
2016-01-16 09:55:16 +0530

    clock js code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 (function () {
     'use strict';
+    var currentTime, d;
+    currentTime = document.querySelector('#currentTime');
+    d = new Date();
+    setInterval(function () {
+        currentTime.innerHTML = d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
+    }, 1000);
 }());
